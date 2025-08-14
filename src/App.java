d1607c59d7231c7b2c1b40c9903d4802a11df62c
João Carlos Viana Teixeira <joaocvteixeira@outlook.com>
2025-08-14 16:55:27 -0300

    Adição do scanner

--- src/App.java
+++ src/App.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         
         //1. Faça um programa em Java que leia dois números e exiba o maior deles.
         
-        int valor1 = 5;
-        int valor2 = 7;
-        int valorMaior = 0;
+        Scanner ler = new Scanner(System.in);
+
+        int valor1;
+        System.out.println("Digite um número: ");
+        valor1 = ler.nextInt();
+
+        int valor2;
+        System.out.println("Digite mais um número: ");
+        valor2 = ler.nextInt();
+
+        int valorMaior;
 
         if (valor1 > valor2) {
             valorMaior = valor1;
